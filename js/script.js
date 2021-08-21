29bcdc60138aee7eff4f483ed6be7e07a0e25b06
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-08-21 22:42:00 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
-function newItem(){
-  let list = $('#list');
+function newItem() {
+  let list = $("#list");
 
-  let li = $('<li></li>');
+  let li = $("<li></li>");
 
-  let inputValue = $('#input').val();
+  let inputValue = $("#input").val();
   li.append(inputValue);
 
+  // To strike items
 
+  li.on("dblclick", function strike() {
+    li.toggleClass("strike");
+  });
 
-// To strike items
+  // display cross crossOutButton
+  let crossOutButton = $("<crossOutButton></<crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
 
-li.on("dblclick",function strike(){
-  li.toggleClass('strike');
-});
+  // to delete the items
 
-// display cross crossOutButton
-let crossOutButton = $('<crossOutButton></<crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-// to delete the items
-
-crossOutButton.on('click',deleteItem);
-function deleteItem(){
-  li.addClass('delete');
-}
-list.append(li);
-// to sort the items
-list.sortable();
+  crossOutButton.on("click", deleteItem);
+  function deleteItem() {
+    li.addClass("delete");
+  }
+  list.append(li);
+  // to sort the items
+  list.sortable();
 }
